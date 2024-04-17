0d4dded302cfc24d9927c8b0fb6bf4bdfc3b776b
Anjan Biswas <anjanavb@amazon.com>
2024-04-16 19:26:11 -0700

    Initial Version

--- src/rhubarb/analyze.py
+++ src/rhubarb/analyze.py
@@ -1,5 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0 
 
 import logging
 from typing import Any, List, Optional, Generator
